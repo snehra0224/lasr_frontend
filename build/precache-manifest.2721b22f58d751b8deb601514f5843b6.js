17e4153606b930ba0828e73bfba7c9e392124e8a
snehra0224 <42956762+snehra0224@users.noreply.github.com>
2021-01-12 14:49:51 -0800

    built app

---
+++ build/precache-manifest.2721b22f58d751b8deb601514f5843b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a266990f5fc2e5847e08ad311225fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10eb25e8fc44d7439920",
+    "url": "/static/css/main.35236c98.chunk.css"
+  },
+  {
+    "revision": "c52823e785055cad4861",
+    "url": "/static/js/2.c3849ad2.chunk.js"
+  },
+  {
+    "revision": "c59b5132dd89a5f1f0fb84ee27bc9c09",
+    "url": "/static/js/2.c3849ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10eb25e8fc44d7439920",
+    "url": "/static/js/main.67cb4ddb.chunk.js"
+  },
+  {
+    "revision": "510bbf2b45e85ad23e61",
+    "url": "/static/js/runtime-main.f6c49df7.js"
+  }
+]);
